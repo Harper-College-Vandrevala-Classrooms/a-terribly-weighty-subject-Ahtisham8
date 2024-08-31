1cdfe79f92852560758f6163e2c9da2401161800
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-08-30 20:55:30 -0700

    Completed the conversion functions
    toPounds() takes in a value in ounces and return the value back in lbs.
    
    toPoundsAndOunces returns the value in pounds and ounces from a the inputted value of just the ounces

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -3,10 +3,40 @@ package com.csc;
 public class Converter {
 
   public String toPounds(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPounds'");
+    String answer = "";
+    double convert;
+
+    convert = (double) ((Math.round((double) ounces*10000)/10000.0) / 16.0000);
+    
+    if (ounces == 16) {
+      answer = String.format("%.4f", convert) + " lb";
+      return answer;
+    }
+
+    
+      answer = String.format("%.4f", convert) + " lbs";
+      return answer;
+    
   }
 
   public String toPoundsAndOunces(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPoundsAndOunces'");
+    String answer = "";
+    int lb;
+    int oz;
+
+    lb = (int) (ounces / 16);
+    oz = (int) (ounces % 16);
+    
+if (lb == 1) {
+  answer = lb + " lb " + oz + " oz";
+  
+  return answer;
+  
+}
+
+  answer= lb + " lbs " + oz + " oz";
+
+  return answer;
+
   }
 }
